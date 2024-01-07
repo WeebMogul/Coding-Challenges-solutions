643ee10ab604a710be7337403562b2c697e1dac4
Clavin D'souza <31620736+WeebMogul@users.noreply.github.com>
2024-01-07 14:35:36 +0400

    Removed click_tutorial.py file and corrected few errors in wc_tool.py

--- Challenge 1 - wc tool/wc_tool.py
+++ Challenge 1 - wc tool/wc_tool.py
@@ -1,11 +1,11 @@
 import click
 import sys
-
+import os
 # Create custom CLI commands
 @click.command()
 
 # CLI argument to add the file or data
-@click.argument('file_data',type=click.File('r'),default=sys.stdin)
+@click.argument('file_data',type=click.File('rb'),default=sys.stdin)
 
 # CLI options to execute when applied 
 @click.option('-c', help = "Display count of bytes in the file", default=False, is_flag=True)
@@ -16,9 +16,13 @@ import sys
 def main(file_data,c,l,w,m):
 
     # check if the file has a name or whether it came from standard input 
-    file_name = file_data.name if file_data.name != "<stdin>" else " "
-
+    file_name = file_data.name
     file_content = file_data.read()
+
+    # If file is read from standard input, encode it to bytes format and process
+    if file_data.name == "<stdin>":
+        file_name = " "
+        file_content = bytes(file_content,'utf-8')
     
     # display count of bytes in the text
     if c:
@@ -35,17 +39,17 @@ def main(file_data,c,l,w,m):
     if not any([c,l,w,m]):
         click.echo(f"{line_count(file_content)} {word_count(file_content)} {char_count(file_content)} {file_name}")
     
-def byte_count(file_name):
-    return len(file_name.encode('utf-8'))
+def byte_count(file_parameter):
+    return len(file_parameter)
 
-def line_count(file_name):
-    return len(file_name.split("\n"))
+def line_count(file_parameter):
+    return len(file_parameter.decode().split("\n"))-1
 
-def word_count(file_name):
-    return len(file_name.split())
+def word_count(file_parameter):
+    return len(file_parameter.decode().split())
 
-def char_count(file_name):
-    return len(file_name)
+def char_count(file_parameter):
+    return len(file_parameter.decode())
 
 if __name__=="__main__":
     main()
